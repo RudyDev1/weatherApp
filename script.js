0ca0fa7446be02394aff958a63ad1c5170e2ad10
RUDYGAMES <rudygamesv2@gmail.com>
2023-03-30 22:02:35 +0200

    added enter listener

--- script.js
+++ script.js
@@ -16,40 +16,53 @@ const getWeather = () => {
 	const city = input.value || 'Bochnia'
 	const URL = API_LINK + city + API_KEY + API_UNITS
 
-	axios.get(URL).then(res => {
-		const temp = res.data.main.temp
-		const hum = res.data.main.humidity
-		const status = Object.assign({}, ...res.data.weather)
-		const statusId = status.id
-		weather.textContent = status.main
+	axios
+		.get(URL)
+		.then(res => {
+			const temp = res.data.main.temp
+			const hum = res.data.main.humidity
+			const status = Object.assign({}, ...res.data.weather)
+			const statusId = status.id
+			weather.textContent = status.main
 
-		if (statusId >= 200 && statusId <= 232) {
-			photo.setAttribute('src', './img/thunderstorm.png')
-		} else if (statusId >= 300 && statusId <= 321) {
-			photo.setAttribute('src', './img/drizzle.png')
-		}else if (statusId >= 500 && statusId <= 531) {
-			photo.setAttribute('src', './img/rain.png')
-		}else if (statusId >= 600 && statusId <= 622) {
-			photo.setAttribute('src', './img/ice.png')
-		}else if (statusId >= 701 && statusId <= 781) {
-			photo.setAttribute('src', './img/fog.png')
-		}else if (statusId == 800) {
-			photo.setAttribute('src', './img/sun.png')
-		}else if (statusId >= 801 && statusId <= 804) {
-			photo.setAttribute('src', './img/cloud.png')
-		}
-        else{
-			photo.setAttribute('src', './img/unknown.png')
-        }
+			warning.textContent = ''
+			input.value = ''
 
-		cityName.textContent = res.data.name
 
-		temperature.textContent = Math.floor(temp) + '℃' // °C
-		humidity.textContent = hum + '%'
+			if (statusId >= 200 && statusId <= 232) {
+				photo.setAttribute('src', './img/thunderstorm.png')
+			} else if (statusId >= 300 && statusId < 400) {
+				photo.setAttribute('src', './img/drizzle.png')
+			} else if (statusId >= 500 && statusId < 600) {
+				photo.setAttribute('src', './img/rain.png')
+			} else if (statusId >= 600 && statusId < 700) {
+				photo.setAttribute('src', './img/ice.png')
+			} else if (statusId >= 700 && statusId < 800) {
+				photo.setAttribute('src', './img/fog.png')
+			} else if (statusId === 800) {
+				photo.setAttribute('src', './img/sun.png')
+			} else if (statusId > 800 && statusId < 900) {
+				photo.setAttribute('src', './img/cloud.png')
+			} else {
+				photo.setAttribute('src', './img/unknown.png')
+			}
 
-		// console.log(res.data.weather[0].id)
-	})
+			cityName.textContent = res.data.name
+
+			temperature.textContent = Math.floor(temp) + '℃' // °C
+			humidity.textContent = hum + '%'
+
+			// console.log(res.data.weather[0].id)
+		})
+		.catch(() => (warning.textContent = 'Wpisz poprawną nazwę miasta'))
+}
+
+const enterCheck = (e) => {
+	if(e.key === 'Enter') {
+		getWeather()
+	}
 }
 
 getWeather()
+input.addEventListener('keydown', enterCheck)
 button.addEventListener('click', getWeather)
